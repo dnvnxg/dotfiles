5be36ed15cfe34e9abec6efe4f5236f0e7d4e909
Donovan Griego <dxgriego@gmail.com>
2026-02-18 13:03:00 -0700

    Revert dynamic hugo_section - caused path issues

--- .emacs.d/ci-build.el
+++ .emacs.d/ci-build.el
@@ -15,23 +15,9 @@
 (require 'ox-hugo)
 (require 'org)
 
-;; 3. Helper function to compute hugo_section from file path
-(defun get-hugo-section-for-file (file base-dir)
-  "Get the hugo_section for a file based on its directory relative to base-dir.
-   
-   Examples:
-   - my-org-files/index.org -> \".\"
-   - my-org-files/cue/cue.org -> \"cue\"
-   - my-org-files/cue/blog/001.org -> \"cue/blog\""
-  (let* ((rel-path (file-relative-name file base-dir))
-         (dir-path (file-name-directory rel-path)))
-    (if (or (null dir-path) (string= dir-path "./"))
-        "."
-      (directory-file-name dir-path))))
-
-;; 4. Define the Build Function
+;; 3. Define the Build Function
 (defun build-quartz-site ()
-  "Export org files to hugo markdown, preserving directory structure."
+  "Export org files to hugo markdown, skipping invalid files."
   (message "Starting Quartz Export...")
   
   ;; --- CONFIGURATION START ---
@@ -39,12 +25,12 @@
   (setq org-hugo-auto-set-lastmod t)
   (setq org-hugo-front-matter-format "yaml")
   
-  ;; 2. Set base directory
+  ;; 2. THE FIX: Tell ox-hugo where the "root" of the site is.
+  ;; We set it to the directory where the org files are checked out.
   (setq org-hugo-base-dir (file-truename "my-org-files"))
   ;; --- CONFIGURATION END ---
   
-  (let ((org-files (directory-files-recursively "my-org-files" "\\.org$"))
-        (base-dir (file-truename "my-org-files")))
+  (let ((org-files (directory-files-recursively "my-org-files" "\\.org$")))
     (dolist (file org-files)
       ;; Skip .github folder and any file in a "finance" directory
       (unless (or (string-match-p "/\\.github/" file)
@@ -55,13 +41,9 @@
                     (goto-char (point-min))
                     (re-search-forward "^#\\+title:" nil t))
                   (progn
-                    ;; Compute and set hugo_section dynamically
-                    (let ((hugo-section (get-hugo-section-for-file file base-dir)))
-                      (message "Exporting: %s (section: %s)" file hugo-section)
-                      ;; Set the hugo_section property at file level
-                      (setq-local org-hugo-section hugo-section)
-                      ;; Export!
-                      (org-hugo-export-wim-to-md :all-subtrees)))
+                    (message "Exporting: %s" file)
+                    ;; Export!
+                    (org-hugo-export-wim-to-md :all-subtrees))
                 (message "Skipping: %s (No #+title found)" file))
             ;; Error handler
             (error (message "ERROR processing %s: %s" file (error-message-string err))))
