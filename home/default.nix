4d1884d1b1313b44691ea71e23c060ab79f6de70
Donovan Xavier Griego <dxgriego@gmail.com>
2026-03-28 12:08:54 -0600

    fix gpg-agent ssh auth for yubikey with nix
    Pin gpgconf to nix store path to avoid homebrew version conflict,
    add sshcontrol keygrips, use pinentry-mac, and updatestartuptty.

--- home/default.nix
+++ home/default.nix
@@ -8,6 +8,9 @@
     settings = {
       group = "me = DFEE781D6F6CA99B! 60AFB3575B788937!";
     };
+    publicKeys = [
+      { source = ../keys/gpg-public-key.asc; trust = 5; }
+    ];
   };
 
   programs.git = {
@@ -26,20 +29,22 @@
   };
 
   home.file.".gnupg/gpg-agent.conf".text = ''
-    pinentry-program ${pkgs.pinentry-curses}/bin/pinentry-curses
+    pinentry-program ${pkgs.pinentry_mac}/bin/pinentry-mac
     enable-ssh-support
   '';
 
-  home.activation.importGpgKey = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-    ${pkgs.gnupg}/bin/gpg --import ${../keys/gpg-public-key.asc} 2>/dev/null || true
+  home.file.".gnupg/sshcontrol".text = ''
+    FCFAB8956F7D22D724ADBCDF115E9F19FB57ACAC
+    C8338759CFB21F019F81EC429D77B0119309B030
   '';
 
   programs.zsh = {
     enable = true;
     initContent = ''
       export GPG_TTY=$(tty)
-      export SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
-      gpgconf --launch gpg-agent
+      export SSH_AUTH_SOCK=$(${pkgs.gnupg}/bin/gpgconf --list-dirs agent-ssh-socket)
+      ${pkgs.gnupg}/bin/gpgconf --launch gpg-agent
+      ${pkgs.gnupg}/bin/gpg-connect-agent updatestartuptty /bye >/dev/null 2>&1
     '';
   };
 }
