5525c18c0c3540694e1d68718515cf9fa739b38a
Donovan Xavier Griego <dxgriego@gmail.com>
2026-04-02 17:13:54 -0600

    Updates and making improvements

--- hosts/macbook/configuration.nix
+++ hosts/macbook/configuration.nix
@@ -3,6 +3,7 @@
 
   environment.systemPackages = with pkgs; [
     yubikey-manager
+    claude-code
   ];
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
@@ -10,6 +11,18 @@
   security.sudo.extraConfig = ''
     dxgriego ALL=(ALL) NOPASSWD: /nix/store/*/bin/darwin-rebuild
   '';
+
+  homebrew = {
+    enable = true;
+    casks = [
+      "claude"
+      "discord"
+      "emacs"
+      "godot"
+      "scroll-reverser"
+      "yubico-yubikey-manager"
+    ];
+  };
 
   system.stateVersion = 6;
   nixpkgs.hostPlatform = "aarch64-darwin";
